10a543ebd59865c07c0c0e0ec6c07c49dfa8ff88
Nov.s <novikov.su20@gmail.com>
2023-03-10 15:59:45 +0500

    Add New Functional

--- src/main/java/ru/netology/homeworks/Radio.java
+++ src/main/java/ru/netology/homeworks/Radio.java
@@ -1,65 +1,81 @@
 package ru.netology.homeworks;
 
 public class Radio {
-    private int currentRadNum;
-    private int currentVolume;
-
-/*
-   private int minRadNum = 0;
+    private int minRadNum = 0;
     private int maxRadNum = 9;
     private int minVolume = 0;
-    private int maxVolume = 10;
-*/
+    private int maxVolume = 100;
+    private int currentRadNum = minRadNum;
+    private int currentVolume = minVolume;
 
+    public Radio (int stationsCount) {
+        maxRadNum = stationsCount - 1;
+    }
 
-    public int getCurrentRadNum() {
-        return currentRadNum;
+    public Radio () {
+    }
+
+    public int getMinRadNum() {
+        return minRadNum;
+    }
+    public int getMaxRadNum() {
+        return maxRadNum;
+    }
+    public int getMinVolume() {
+        return minVolume;
+    }
+    public int getMaxVolume() {
+        return maxVolume;
     }
 
     public void setCurrentRadNum(int currentRadNum) {
-        if (currentRadNum < 0 || currentRadNum > 9) {
+        if (currentRadNum < minRadNum || currentRadNum > maxRadNum) {
             return;
         }
         this.currentRadNum = currentRadNum;
 
     }
 
+    public int getCurrentRadNum() {
+        return currentRadNum;
+    }
+
     public void nextRadNum() {
-        if (currentRadNum >= 9) {
-            setCurrentRadNum(0);
+        if (currentRadNum >= maxRadNum) {
+            setCurrentRadNum(minRadNum);
         } else {
             setCurrentRadNum(currentRadNum + 1);
         }
     }
 
     public void prevRadNum() {
-        if (currentRadNum <= 0) {
-            setCurrentRadNum(9);
+        if (currentRadNum <= minRadNum) {
+            setCurrentRadNum(maxRadNum);
         } else {
             setCurrentRadNum(currentRadNum - 1);
         }
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < minVolume || currentVolume > maxVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
     }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0 || currentVolume > 10) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-
     public void increaseVolume() {
-        if (currentVolume < 10) {
+        if (currentVolume < maxVolume) {
             currentVolume = currentVolume + 1;
         }
     }
 
 
     public void decreaseVolume() {
-        if (currentVolume > 0) {
+        if (currentVolume > minVolume) {
             currentVolume = currentVolume - 1;
         }
     }
